0dd0fd61ef7b9e0794ddb61e596b4ba065006fac
Mason Fisher <sackzombie10@gmail.com>
2023-08-16 21:15:48 -0400

    Making qr code with less dependencies and it's bigger now

--- src/showcode.rs
+++ src/showcode.rs
@@ -3,39 +3,58 @@ use gtk4::{Window, Image};
 use gtk4::gdk_pixbuf::{Pixbuf, Colorspace};
 
 use qrcode::QrCode;
-use image::Luma;
+
+const SIZE_MULT: u32 = 32;
 
 pub fn show_code(code: &str) {
     let my_window = Window::new();
     my_window.set_title(Some("QR Code"));
 
     let qr_code = QrCode::new(code).unwrap();
-    let qr_image = qr_code.render::<Luma<u8>>().build();
+    let qr_image = qr_code.render()
+        .light_color('0')
+        .dark_color('1')
+        .build();
 
-    let width = qr_image.width() as i32;
-    let height = qr_image.height() as i32;
+    let lines: Vec<&str> = qr_image.split('\n').collect();
+    let img_size = lines.len() * SIZE_MULT as usize;
 
-    let mut pixbuf = Pixbuf::new(
+    let pixbuf = Pixbuf::new(
         Colorspace::Rgb,
         false,
         8,
-        width,
-        height,
-    ).expect("Failed to create pixbuf from QR code image");
+        img_size as i32,
+        img_size as i32,
+    ).expect("Failed to create pixbuf");
 
-    for y in 0..height {
-        for x in 0..width {
-            let luma = qr_image.get_pixel(x as u32, y as u32)[0];
-            let pixel = ((luma as u32) << 16) | ((luma as u32) << 8) | luma as u32;
-            pixbuf.put_pixel(x as u32,
-                             y as u32,
-                             ((pixel >> 16) & 0xFF) as u8,
-                             ((pixel >> 8) & 0xFF) as u8,
-                             (pixel & 0xFF) as u8, 255);
+    // Put pixels on pixbuf
+    for cy in 0..lines.len() {
+        for cx in 0..lines[cy].len() {
+            if let Some(curr_line) = lines.get(cy) {
+                if let Some(character) = curr_line.chars().nth(cx) {
+                    // Get color
+                    let color;
+                    if character == '1' {
+                        color = 0;  // Black
+                    } else {
+                        color = 255;// White
+                    }
+
+                    // Place pixels
+                    for pix in 0..SIZE_MULT*SIZE_MULT {
+                        let pixy = (pix as f64 / SIZE_MULT as f64).floor() as u32;
+
+                        pixbuf.put_pixel((pix%32) as u32 + cx as u32*SIZE_MULT, pixy + cy as u32*SIZE_MULT, color, color, color, 255);
+                    }
+
+                } else {
+                    panic!("Overflowed QR image string on X");
+                }
+            } else {
+                panic!("Overflowed QR image string on Y");
+            }
         }
     }
-
-    println!("x: {} | y: {}", width, height);
 
     let image_widget = Image::from_pixbuf(Some(&pixbuf));
     my_window.set_child(Some(&image_widget));
