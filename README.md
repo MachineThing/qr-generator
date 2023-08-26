db31d41f9f868201a6987da10c5b1bda94941a93
Mason Fisher <sackzombie10@gmail.com>
2023-08-26 10:41:14 -0400

    Making release zip and appimage

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # QR Code Generator
 
 A QR Code generator written in Rust.
+
+## How to compile
+You will need:
+- A computer running your favorite operating system *(like Ubuntu, Windows, MacOS, etc)*
+- Rust & Cargo
+
+Simply run `cargo build` or `cargo run` (if you want to run the project immediately after compiling it) in the root of the project folder and it should compile.
+
+To make a release version you will need
+- A computer running your favorite Linux distro *(like Debian, Fedora, Arch, etc)*
+- Appimagetool
+- Docker
+
+Simply run `release.sh` in the root of the project folder and it should compile.
---
+++ release.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# This script makes release zips/tar.gzs for github
+sudo rm -rf ./package gh_release ./temp
+
+mkdir -p ./temp/linux ./temp/win
+
+# Linux exec
+cargo build --release --target x86_64-unknown-linux-gnu
+cp ./target/x86_64-unknown-linux-gnu/release/qrgen ./temp/linux/.
+
+# Windows exec
+docker run -v `pwd`:/mnt mglolenstine/gtk4-cross:rust-gtk-4.10 /bin/bash -c "build; package"
+sudo chown $(whoami):$(whoami) -R ./temp ./package
+sudo mv ./package/* ./temp/win/.
+sudo rmdir ./package
+
+# Package it all up
+cp ./LICENSE.md ./temp/win/.
+cp ./README.md ./temp/win/.
+
+mkdir gh_release
+
+ldd ./temp/linux/qrgen | grep -o '/[^[:space:]]*' | awk '{system("cp "$1" ./temp/linux/")}'
+cp ./qrcode.png ./temp/linux/.
+echo "[Desktop Entry]
+Name=QR Code Generator
+Exec=qrgen
+Icon=qrcode
+Type=Application
+Categories=Utility;" > ./temp/linux/qrgen.desktop
+appimagetool ./temp/linux/ --comp xz
+
+mv ./QR_Code_Generator-x86_64.AppImage ./gh_release/qrgen-x86_x64.AppImage
+$(cd ./temp/win; zip -r ../../gh_release/win-x86_64.zip .)
+
+rm -rf ./temp
+echo "\a"
