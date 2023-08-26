69bec8c526b8e8f8c96b28d1dc3d2f8f49060324
Mason Fisher <sackzombie10@gmail.com>
2023-08-26 10:59:54 -0400

    Fix release.sh

--- release.sh
+++ release.sh
@@ -5,26 +5,30 @@ sudo rm -rf ./package gh_release ./temp
 mkdir -p ./temp/linux ./temp/win
 
 # Linux exec
+echo building linux
 cargo build --release --target x86_64-unknown-linux-gnu
-cp ./target/x86_64-unknown-linux-gnu/release/qrgen ./temp/linux/.
+cp ./target/x86_64-unknown-linux-gnu/release/qrgen ./temp/linux/AppRun
+ldd ./temp/linux/AppRun | grep -o '/[^[:space:]]*' | awk '{system("cp "$1" ./temp/linux/")}'
+cp ./qrcode.png ./temp/linux/qrcode.png
+ln -s ./temp/linux/qrcode.png ./temp/linux/.DirIcon
 
 # Windows exec
+echo building windows
 docker run -v `pwd`:/mnt mglolenstine/gtk4-cross:rust-gtk-4.10 /bin/bash -c "build; package"
 sudo chown $(whoami):$(whoami) -R ./temp ./package
 sudo mv ./package/* ./temp/win/.
 sudo rmdir ./package
 
 # Package it all up
+echo packing
 cp ./LICENSE.md ./temp/win/.
 cp ./README.md ./temp/win/.
 
 mkdir gh_release
 
-ldd ./temp/linux/qrgen | grep -o '/[^[:space:]]*' | awk '{system("cp "$1" ./temp/linux/")}'
-cp ./qrcode.png ./temp/linux/.
 echo "[Desktop Entry]
 Name=QR Code Generator
-Exec=qrgen
+Exec=AppRun
 Icon=qrcode
 Type=Application
 Categories=Utility;" > ./temp/linux/qrgen.desktop
@@ -34,4 +38,4 @@ mv ./QR_Code_Generator-x86_64.AppImage ./gh_release/qrgen-x86_x64.AppImage
 $(cd ./temp/win; zip -r ../../gh_release/win-x86_64.zip .)
 
 rm -rf ./temp
-echo "\a"
+echo "Done!"
